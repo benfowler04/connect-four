5fb3cfe0d3470f4e7ce4e0828464acb8531cb7ba
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-08-24 18:12:32 -0400

    completed ConnectFour tests and class

--- game.rb
+++ game.rb
@@ -1,1 +1,10 @@
 require './lib/connect_four'
+
+puts "Player 1, what's your name?"
+player1 = gets.chomp
+puts
+puts "Player 2, what's your name?"
+player2 = gets.chomp
+
+game = ConnectFour.new(player1, player2)
+game.play
--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,2 +1,40 @@
 require './lib/board'
 require './lib/players'
+
+class ConnectFour
+    def initialize(player1, player2)
+        @players = Players.new(player1, player2)
+        @board = Board.new
+    end
+
+    def play
+        game_over = false
+        until game_over
+            valid_move = false
+            until valid_move
+                puts
+                @board.show_board
+                puts
+                puts "It's #{@players.current_player}'s turn. Select a column (1-7 starting from left)"
+                column = gets.chomp.to_i
+                valid_move = true if @board.is_move_valid?(column - 1)
+            end
+            result = @board.drop_token(column - 1, @players.current_token)
+            if result == -1
+                puts
+                @board.show_board
+                puts
+                puts "It's a draw."
+                game_over = true
+            elsif result == 1
+                puts
+                @board.show_board
+                puts
+                puts "#{@players.current_player} wins!"
+                game_over = true
+            else
+                @players.switch_player
+            end
+        end
+    end
+end
