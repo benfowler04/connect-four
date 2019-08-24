2e8d98a976638ddb6b2f5d036ea573df5860f627
Ben Fowler <46171044+benfowler04@users.noreply.github.com>
2019-08-24 17:39:29 -0400

    completed Board tests and most of Board class

---
+++ lib/board.rb
@@ -0,0 +1,74 @@
+class Board
+    attr_reader :current_state
+    
+    def initialize
+        @moves = 0
+        @current_state = Array.new(7){Array.new(6)}
+        for column in 0..6
+            for row in 0..5
+                @current_state[column][row] = "o"
+            end
+        end
+    end
+
+    def is_move_valid?(column)
+        return false if column < 0 || column > 6
+        return false unless @current_state[column][5] == "o"
+        return true
+    end
+
+    def drop_token(column, color)
+        row = @current_state[column].index("o")
+        @current_state[column][row] = color
+        @moves += 1
+        return 1 if winning_move?(column, row)
+        return -1 if draw?
+        return 0
+    end
+
+    def show_board
+        for row in [5,4,3,2,1,0]
+            for column in 0..6
+                print @current_state[column][row]
+            end
+            print "\n"
+        end
+    end
+
+    private
+    def draw?
+        @moves == 42
+    end
+
+    def winning_move?(column, row)
+        winning_column?(column) || winning_row?(row) || winning_diagonal?(column, row)
+    end
+
+    def winning_column?(column)
+        return false if @current_state[column].count("o") > 2
+        possible_wins = []
+        possible_wins.push(@current_state[column][0..3].uniq.length)
+        possible_wins.push(@current_state[column][1..4].uniq.length)
+        possible_wins.push(@current_state[column][2..5].uniq.length)
+        return true if possible_wins.include?(1)
+        return false
+    end
+
+    def winning_row?(row)
+        row_contents = []
+        for column in 0..6
+            row_contents.push(@current_state[column][row])
+        end
+        return false if row_contents.count("o") > 3
+        possible_wins = []
+        possible_wins.push(row_contents[0..3].uniq.length)
+        possible_wins.push(row_contents[1..4].uniq.length)
+        possible_wins.push(row_contents[2..5].uniq.length)
+        possible_wins.push(row_contents[3..6].uniq.length)
+        return true if possible_wins.include?(1)
+        return false
+    end
+
+    def winning_diagonal?(column, row)
+    end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,112 @@
 require './lib/board'
+
+RSpec.describe Board do
+    describe "#initialize" do
+        it "creates an empty board" do
+            board = Board.new
+            expect(board.current_state.uniq.length).to eql(1)
+        end
+        it "creates 7 columns" do
+            board = Board.new
+            expect(board.current_state.length).to eql(7)
+        end
+        it "creates 6 rows" do
+            board = Board.new
+            expect(board.current_state[0].length).to eql(6)
+        end
+    end
+
+    describe "#is_move_valid?" do
+        it "returns false if column is out of bounds" do
+            board = Board.new
+            expect(board.is_move_valid?(7)).to eql(false)
+        end
+        it "returns false if row is full" do
+            board = Board.new
+            3.times { board.drop_token(0, "B") }
+            3.times { board.drop_token(0, "R") }
+            expect(board.is_move_valid?(0)).to eql(false)
+        end
+        it "returns true if column is empty" do
+            board = Board.new
+            expect(board.is_move_valid?(0)).to eql(true)
+        end
+        it "returns true if column is neither empty nor full" do
+            board = Board.new
+            2.times { board.drop_token(0, "B") }
+            expect(board.is_move_valid?(0)).to eql(true)
+        end
+    end
+
+    describe "#drop_token" do
+        it "returns 1 if this completes 4 in a column" do
+            board = Board.new
+            3.times { board.drop_token(0, "B") }
+            expect(board.drop_token(0, "B")).to eql(1)
+        end
+        it "returns 1 if this completes 4 in a row" do
+            board = Board.new
+            board.drop_token(3, "B")
+            board.drop_token(2, "B")
+            board.drop_token(1, "B")
+            expect(board.drop_token(0, "B")).to eql(1)
+        end
+        it "returns 1 if this completes 4 in a diagonal" do
+            board = Board.new
+            board.drop_token(0, "B")
+            board.drop_token(1, "R")
+            board.drop_token(1, "B")
+            2.times { board.drop_token(2, "R") }
+            3.times { board.drop_token(3, "R") }
+            board.drop_token(3, "B")
+            expect(board.drop_token(2, "B")).to eql(1)
+        end
+        it "returns 0 if the game should continue" do
+            board = Board.new
+            board.drop_token(0, "B")
+            expect(board.drop_token(0, "R")).to eql(0)
+        end
+        it "returns -1 if the game ends in a draw" do
+            board = Board.new
+            3.times { board.drop_token(0, "B") }
+            3.times { board.drop_token(0, "R") }
+            3.times { board.drop_token(1, "R") }
+            3.times { board.drop_token(1, "B") }
+            3.times { board.drop_token(2, "B") }
+            3.times { board.drop_token(2, "R") }
+            3.times { board.drop_token(3, "R") }
+            3.times { board.drop_token(3, "B") }
+            3.times { board.drop_token(4, "B") }
+            3.times { board.drop_token(4, "R") }
+            3.times { board.drop_token(5, "R") }
+            3.times { board.drop_token(5, "B") }
+            3.times { board.drop_token(6, "B") }
+            2.times { board.drop_token(6, "R") }
+            expect(board.drop_token(6, "R")).to eql(-1)
+        end
+        it "updates the game board when a token is dropped into an empty column" do
+            board = Board.new
+            board.drop_token(0, "B")
+            expect(board.current_state[0][0]).to eql("B")
+        end
+        it "updates the game board when a token is dropped into a non-empty column" do
+            board = Board.new
+            board.drop_token(0, "B")
+            board.drop_token(0, "R")
+            expect(board.current_state[0][1]).to eql("R")
+        end
+    end
+
+    describe "#show_board" do
+        it "outputs an empty board" do
+            board = Board.new
+            expect{board.show_board}.to output("ooooooo\nooooooo\nooooooo\nooooooo\nooooooo\nooooooo\n").to_stdout
+        end
+        it "outputs a board with tokens" do
+            board = Board.new
+            board.drop_token(0, "B")
+            board.drop_token(0, "R")
+            expect{board.show_board}.to output("ooooooo\nooooooo\nooooooo\nooooooo\nRoooooo\nBoooooo\n").to_stdout
+        end
+    end
+end
